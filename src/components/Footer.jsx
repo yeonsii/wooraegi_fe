44ddab1fe739c24fedbdbb6682db8a335a382479
yeonsii <37923273+yeonsii@users.noreply.github.com>
2024-03-06 13:34:10 +0900

    completed footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer id="Footer" role="contentinfo">
+            <div class="footer__inner">
+                <div class="footer__text">
+                    <span>© wooraegi</span>
+                </div>
+                <div class="footer__info">
+                  <div>
+                    업체명 : (주)우래기 | 주소 : 서울특별시 강남구
+                    <br />
+                    이메일 : wooraegi@gmail.com | 전화번호 : 070-123-1234 
+                  </div>
+                </div>
+                <div class="footer__right">
+                    © wooraegi<br />
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
