9b3635c91da62bd1b9f4245fc2f3100f94786a19
yeonsii <37923273+yeonsii@users.noreply.github.com>
2024-03-06 13:15:47 +0900

    completed header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+    {
+        title: "main",
+        url: "#intro",
+    },
+    {
+        title: "diary",
+        url: "#diary",
+    },
+    {
+        title: "log",
+        url: "#log",
+    },
+    {
+        title: "profile",
+        url: "#profile",
+    },
+];
 
 const Header = () => {
-    return <div>Header</div>;
+
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
+    return (
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">wooraegi</a>
+                    <br />
+                    우리애기모음.<em>zip</em>
+                </div>
+                <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+                     <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div 
+                    className="header__nav__mobile" 
+                    id="headerToggle" 
+                    aria-controls="primary-menu" 
+                    aria-expanded={show ? "true" : "false"}
+                    role="button" 
+                    tabindex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
