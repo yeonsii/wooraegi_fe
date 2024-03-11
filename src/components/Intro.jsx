d604b57b843e0e221cf56f7cff5c234f96408ad7
yeonsii <37923273+yeonsii@users.noreply.github.com>
2024-03-11 12:36:14 +0900

    setting

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Intro = () => {
-    return <div>Intro</div>;
+    return (
+        <div className="intro_inner">
+                <input className="intro_search" type="text" placeholder="검색어를 입력하세요"></input>
+        </div>
+    );
 };
 
 export default Intro;
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -3,9 +3,6 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Main from "../components/Main"
 import Intro from "../components/Intro"
-import Diary from "../components/Diary"
-import Log from "../components/Log"
-import Profile from "../components/Profile"
 
 const HomeView = () => {
     return (
@@ -13,9 +10,6 @@ const HomeView = () => {
         <Header />
             <Main>
                 <Intro />
-                <Diary />
-                <Log />
-                <Profile />
             </Main>
         <Footer />
     </>
